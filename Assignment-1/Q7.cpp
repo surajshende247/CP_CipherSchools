db4531ac2ef4f9ddcb5666ebbb10266f6f4979c9
root <root@DESKTOP-K48UMI1.localdomain>
2021-02-14 13:04:53 +0530

    Q7 Done

--- Assignment-1/Q7.cpp
+++ Assignment-1/Q7.cpp
@@ -5,9 +5,10 @@ void alternateSort(int arr[], int n)
 { 
 	sort(arr, arr+n);
 	int i = 0, j = n-1; 
-	while (i < j) { 
-		cout << arr[j--] << " "; 
-		cout << arr[i++] << " "; 
+	while (i < j) 
+    { 
+		cout << arr[j--] <<" "; 
+		cout << arr[i++] <<" "; 
 	} 
 	if (n % 2 != 0) 
 		cout << arr[i]; 
