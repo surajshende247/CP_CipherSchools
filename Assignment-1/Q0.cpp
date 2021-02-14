9cb4bd1e1f12fdcc4a7a27806e0900b9d487490d
surajshende247 <surajshende247@gmail.com>
2021-02-14 09:00:07 +0530

    Q1 Done

---
+++ Assignment-1/Q0.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+bool everMeet(int x1, int x2, int v1, int v2) 
+{ 
+    if (x1 < x2 && v1 <= v2) 
+       return false; 
+    if (x1 > x2 && v1 >=v2) 
+       return false;   
+    
+    if (x1 < x2)  
+    {  
+        swap(x1, x2);  
+        swap(v1, v2);  
+    }      
+     while (x1 >= x2) {  
+        if (x1 == x2)  
+            return true; 
+        x1 = x1 + v1;  
+        x2 = x2 + v2;  
+    } 
+  
+    return false;    
+} 
+  
+int main() 
+{ 
+    int x1 = 5, v1 = 8, x2 = 4, v2 = 7; 
+    if (everMeet(x1, x2, v1, v2)) 
+        cout<<"Yes";     
+    else 
+        cout<<"No"; 
+    return 0; 
+} 
