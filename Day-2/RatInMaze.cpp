632f4cbda869194351ea9845b54b2dbf72164fb7
root <root@DESKTOP-K48UMI1.localdomain>
2021-02-14 12:45:52 +0530

    Q3 Done

---
+++ Day-2/RatInMaze.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSafe(vector<vector<int>>& grid)
+
+bool ratInMaze(vector<vector<int>>& grid, vector<vector<int>> &visited, int x, int y, int R, int C){
+    if(x == R-1 && y == C-1 && grid[x][y] == 0){
+        visited[x][y] = true;
+    }
+}
+
+bool ratInMaze(vector<vector<int>> grid){
+
+}
+
+int main(){
+    return 0;
+}
